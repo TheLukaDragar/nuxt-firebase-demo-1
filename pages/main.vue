aecd60036b79c8b060b8348f09586bcc9a469c4f
LukaDragar <lukadragarbusiness@gmail.com>
2021-07-29 21:07:19 +0200

    add main page

---
+++ pages/main.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <v-container>
+
+    <h3 class="text-3xl mb-5">
+        Test
+    </h3>
+    
+    
+
+  </v-container>
+
+
+</template>
+
+<script>
+
+
+
+
+export default {
+
+  layout: "main",
+
+  components: {
+    
+  },
+
+}
+</script>
