75b72d0a83268ed5e4c86904d8c9502060489d6d
LukaDragar <lukadragarbusiness@gmail.com>
2021-08-02 12:55:37 +0200

    no go for hosting in functions

--- functions/index.js
+++ functions/index.js
@@ -4,36 +4,7 @@ admin.initializeApp()
 const messaging = admin.messaging()
 const db = admin.firestore();
 
-//////////////
-const { Nuxt } = require("nuxt-start");
 
-const nuxtConfig = require("./nuxt.config.js");
-
-const config = {
-  ...nuxtConfig,
-  dev: false,
-  debug: false,
-  buildDir: ".nuxt",
-  publicPath: "public"
-};
-
-const nuxt = new Nuxt(config);
-
-let isReady = false;
-
-async function handleRequest(req, res) {
-  if (!isReady) {
-    try {
-      isReady = await nuxt.ready();
-    } catch (error) {
-      process.exit(1);
-    }
-  }
-  await nuxt.render(req, res);
-}
-
-exports.ssrapp = functions.https.onRequest(handleRequest);
-///////////////
 
 exports.testFunction = functions.https.onCall(() => {
   console.info('Test Function triggered')
